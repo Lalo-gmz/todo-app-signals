d9efec60b71ea98defa1ad6f82314ee38ad1370e
eduardogomez <uvangel.gomez@gmail.com>
2023-08-06 19:11:17 -0600

    todo completed

--- src/app/todos/components/main/main.component.ts
+++ src/app/todos/components/main/main.component.ts
@@ -13,6 +13,7 @@ import { TodoComponent } from "../todo/todo.component";
 
 export class MainComponent {
     todosService = inject(TodoService)
+    editingId: string | null = null;
 
     visibleTodos = computed(()=> {
         const todos = this.todosService.todosSig()
@@ -24,6 +25,17 @@ export class MainComponent {
             return todos.filter((todo) => todo.isCompleted);
         }
         return todos;
-
     })
+    isAllTodosSelected = computed(()=> this.todosService.todosSig().every(todo => todo.isCompleted))
+
+    noTodosClass = computed(() => this.todosService.todosSig().length === 0)
+    
+    setEditingId(editingId: string | null): void {
+        this.editingId = editingId;
+    }
+
+    toggleAllTodos(event: Event): void {
+        const target = event.target as HTMLInputElement
+        this.todosService.toggleAll(target.checked)
+    }
 }
--- src/app/todos/services/todo.service.ts
+++ src/app/todos/services/todo.service.ts
@@ -23,4 +23,26 @@ export class TodoService {
 		this.todosSig.update((todos) => [...todos, newTodo]);
 	}
 	
+	changeTodo(id: string, text: string): void {
+		this.todosSig.update((todos) => todos.map((todo) => (todo.id === id ? {...todo, text} : todo)))
+	}
+
+	removeTodo(id: string): void {
+		this.todosSig.update((todos)=> todos.filter((todo)=> todo.id !== id));
+	}
+
+	toggleTodo(id: string): void {
+		this.todosSig.update((todos) => 
+			todos.map((todo) => 
+				todo.id === id ? {...todo, isCompleted: !todo.isCompleted} : todo
+			)
+		);
+	}
+
+	toggleAll(isCompleted: boolean): void {
+		this.todosSig.update((todos) => 
+		todos.map((todo) => ({...todo, isCompleted} )
+		)
+	);
+	}
 }
